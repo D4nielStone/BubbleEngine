ece60918c924cc859c6a2ed919223678eebe1204
D4niel Stone <do1167475@gmail.com>
2025-01-28 14:12:10 -0300

    Atualização automática

--- golf/cam.lua
+++ golf/cam.lua
@@ -1,14 +1,33 @@
 -- Autor Daniel Oliveira (c) 2025 --
+
 function iniciar()
     print("Iniciando o jogo...")
 end
+
 function atualizar()
-    eu.transformacao.rotacao.y = inputs:mouse().x
+    -- Suaviza a rotação da câmera com base no movimento do mouse
+    local sensibilidade = 0.1
+    eu.transformacao.rotacao.y = eu.transformacao.rotacao.y + inputs:mouse().x * sensibilidade
+    eu.transformacao.rotacao.x = eu.transformacao.rotacao.x - inputs:mouse().y * sensibilidade
+    
+    -- Vetor de direção baseado na rotação da câmera
+    local direcao_frente = eu.camera:frente()
+    local direcao_direita = eu.camera:direita()
+    local velocidade = 0.5
+    
+    -- Movimento para frente e para trás
     if inputs.pressionada("W") then
-    eu.camera:mover(vetor3(0, 0, 0.5))
+        eu.camera:mover(direcao_frente * velocidade)
     end
     if inputs.pressionada("S") then
-        eu.camera:mover(vetor3(0, 0, -0.5))
-        end
-    print("A")
+        eu.camera:mover(direcao_frente * -velocidade)
+    end
+    
+    -- Movimento lateral
+    if inputs.pressionada("A") then
+        eu.camera:mover(direcao_direita * -velocidade)
+    end
+    if inputs.pressionada("D") then
+        eu.camera:mover(direcao_direita * velocidade)
+    end
 end
